f5eb06876d3f99b0fb6a7edff26042a7a5d8c873
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-02-02 19:23:28 +0700

    case 3

--- Qualification/convert.py
+++ Qualification/convert.py
@@ -2,8 +2,6 @@ import csv
 
 with open('Qualification/SMS_train.csv', newline='', encoding='latin-1') as csvfile:
     reader = csv.DictReader(csvfile)
-    pos = 0;
-    neg = 0;
     
     for row in reader:
         try:
@@ -16,7 +14,6 @@ with open('Qualification/SMS_train.csv', newline='', encoding='latin-1') as csvf
         if label == "Non-Spam":
             with open('pos.txt', 'a', encoding='utf-8') as file:
                 file.write(review + '\n')
-                pos = pos + 1;
         elif label == "Spam":
             with open('neg.txt', 'a', encoding='utf-8') as file:
                 file.write(review + '\n')
